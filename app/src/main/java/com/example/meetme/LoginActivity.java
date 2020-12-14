d201d5d0f123f5946e367664aee85877fd636bad
tal zaidman <Talzaidman5@gmail.com>
2020-12-14 21:00:47 +0200

    new

--- app/src/main/java/com/example/meetme/LoginActivity.java
+++ app/src/main/java/com/example/meetme/LoginActivity.java
@@ -10,11 +10,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class LoginActivity extends AppCompatDialogFragment {
     private View view;
     private EditText login_EDT_Email,login_EDT_Password;
+    private Button cirLoginButton;
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
@@ -25,21 +27,28 @@ public class LoginActivity extends AppCompatDialogFragment {
         final AlertDialog dialog = builder.create();
         login_EDT_Email = view.findViewById(R.id.login_EDT_Email);
         login_EDT_Password = view.findViewById(R.id.login_EDT_Password);
-     //   if(checkDetails()){
-            Intent intent = new Intent(LoginActivity.this.getContext(),MatchingActivity.class);
-            startActivity(intent);
-     //   }
-        
+        cirLoginButton = view.findViewById(R.id.cirLoginButton);
+        cirLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(checkDetails()){
+                    Intent intent = new Intent(LoginActivity.this.getContext(),MatchingActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
         return dialog;
     }
 
     private boolean checkDetails() {
 
         for (User tempUser:MainActivity.allClients.allClientsInDB) {
-            if(tempUser.getEmail().equals(login_EDT_Email.getText().toString()))
-                if(tempUser.getPassword().equals(login_EDT_Password.getText().toString()))
-                    return true;
-
+            if(tempUser!=null) {
+                if (tempUser.getEmail().equals(login_EDT_Email.getText().toString()))
+                    if (tempUser.getPassword().equals(login_EDT_Password.getText().toString()))
+                        return true;
+            }
         }
     return false;
     }
--- app/src/main/java/com/example/meetme/MatchingActivity.java
+++ app/src/main/java/com/example/meetme/MatchingActivity.java
@@ -6,11 +6,13 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MatchingActivity extends AppCompatActivity {
     private RecyclerView main_LST_news;
+    private ArrayList<User> users;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +21,11 @@ public class MatchingActivity extends AppCompatActivity {
 
         main_LST_news = findViewById(R.id.main_LST_news);
 
-        ArrayList<User> users = MainActivity.allClients.getAllClientsInDB();
+         if(MainActivity.allClients.getAllClientsInDB()!=null)
+              users = MainActivity.allClients.getAllClientsInDB();
+         else
+             Toast.makeText(MatchingActivity.this, "אין נתונים", Toast.LENGTH_SHORT).show();
+
         Adapter_User adapter_article = new Adapter_User(this, users);
         main_LST_news.setLayoutManager(new LinearLayoutManager(this));
         main_LST_news.setItemAnimator(new DefaultItemAnimator());
